ead770f891b6d05d6de61b71f0534afcdd44b02c
rarrara <dangeryt2004@gmail.com>
2024-02-08 14:39:41 +0530

    new

---
+++ EvenOddNumbers.java
@@ -0,0 +1,14 @@
+package Endsem;
+
+public class EvenOddNumbers {
+public static void main(String[] args) {
+System.out.println("Even numbers:"); for (int i = 100; i <= 150; i++) {
+if (i % 2 == 0) {
+System.out.print(i + " "); }
+}
+System.out.println("\nOdd numbers:"); for (int i = 100; i <= 150; i++) {
+if (i % 2 != 0) {
+System.out.print(i + " "); }
+} }
+
+}
